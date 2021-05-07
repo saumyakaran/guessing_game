ad1876a68da24ee11d6e1ac06008cd8fe80dd37e
saumyakaran <40575379+saumyakaran@users.noreply.github.com>
2021-05-07 17:38:10 +0530

    hotfix for player instructions

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    println!("Guess the number!");
+    println!("Guess an integer between 1 and 100!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
