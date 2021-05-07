561d1cc5eab422d7bffb5a94d85503aaddf05079
saumyakaran <40575379+saumyakaran@users.noreply.github.com>
2021-05-07 17:37:01 +0530

    🎉 v1 release

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,43 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
 
-    println!("Please input your guess.");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    let mut guess = String::new();
+    loop {
+        println!("💁🏼‍ Please input your guess.");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        let mut guess = String::new();
 
-    println!("You guessed: {}", guess);
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        if guess == "quit" {
+            println!("Goodbye... 👋🏻");
+            break;
+        }
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("\n===========================================\n🚧 Input must be a integer below 1 and 100!\n===========================================\n\n");
+                continue;
+            },
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("\n🤭 Too low!\n-----------\n"),
+            Ordering::Greater => println!("\n😲 Too high!\n-----------\n"),
+            Ordering::Equal => {
+                println!("\n======================\n\n🎉 Perfect! You win 🏆\n\n\n");
+                break;
+            }
+        }
+    }
 }
